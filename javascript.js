90eb683645e2757ad394d44d0a4b8b998992834c
MalvexX <71018760+MalvexX@users.noreply.github.com>
2020-10-14 22:25:22 +0200

    0.12 - Simultaneous search

--- javascript.js
+++ javascript.js
@@ -1,126 +1,135 @@
-const pagetitle = "DEV - Flagfilter - Quick and easy keyword search for flags";
+/*
 
-/* Filter by buttons */
-function filterSelection(input) {
-	document.getElementById("myInput").value = "";	//clear text search field
-	
-	//set title to search terms
-	if (input == "all") {
-		document.title = pagetitle;
-	} else {
-		let buttonpressed = document.getElementsByClassName("filterbutton" + " " + input);
-		document.title = "Flagfilter - " + buttonpressed[0].innerHTML;
-	}			
-	
-	classFilter(input);
-}
+Flagfilter.com
+Version 0.12
+
+*/
+
+const pageTitle = "Flagfilter - Quick and easy keyword search for flags";
+
+// Autofocus the text input field
+let inputField = document.getElementById("myInput");
+inputField.focus();
+
+var buttonArray = new Array();	// Button search terms
+var textInput = "";
 
 /* Filter by input text field */
 function textFilter() {
-	console.log("text input entered");
-	
-	let rawinput = document.getElementById("myInput");
-	let input = rawinput.value.toLowerCase().trim();
-	
-	console.log("input.length: " + input.length);
-	
-	//clear active button
-	if (input.length > 0) {
-		let current = document.getElementsByClassName("active");
-		if (current.length > 0) {
-			for (let i = 0; i < current.length; i++) {
-				current[i].classList.remove("active");
-			}
-		}
-	}
+	let raw = document.getElementById("myInput");
+	textInput = raw.value.toLowerCase().trim();
 
-	//set title to search terms
-	if (input.length > 0) {
-		document.title = "Flagfilter - " + input;
-	} else {
-		document.title = pagetitle;
-	}
+	console.log("textInput: " + textInput);
 	
-	classFilter(input);
+	showFlags();
 }
 
 function classFilter(input) {
 
-	console.log("Input: \"" + input + "\"");			
-	
-	if (!input.trim().length || input== "all") {
-		//show all flags if no input or "Show all" button is pressed
-		let allflags = document.getElementsByClassName("flag");
-		for (let i = 0; i < allflags.length; i++) {
-			allflags[i].style.display = "";
-		}
+	// Testing if input is new and should be added to buttonArray
+	if (buttonArray.indexOf(input) == -1){
+		buttonArray.push(input);	// Add input to buttonArray
 	} else {
-		//hide all flags and show only matching flags
-		let allflags = document.getElementsByClassName("flag");
-		for (let i = 0; i < allflags.length; i++) {
-			allflags[i].style.display = "none";
-		}
-		let visibleflags = document.getElementsByClassName(input);
-		for (let i = 0; i < visibleflags.length; i++) {
-			visibleflags[i].style.display = "";
+		buttonArray.splice(buttonArray.indexOf(input), 1);	// Removing input from buttonArray
+	}
+
+	// Show all flags if buttonArray is empty
+	if (buttonArray.length == 0 || input == "all"){
+		buttonArray = [];	// Reset buttonArray
+		document.getElementById("myInput").value = "";		// Clear text search field
+
+		setActiveButton("all");
+		showAllFlags();
+	} else {
+		hideAllFlags();
+		setActiveButton(input);
+		showFlags();
+	}
+}
+
+// Hiding all flags
+function hideAllFlags() {
+	let allFlags = document.getElementsByClassName("flag");
+	for (let i = 0; i < allFlags.length; i++) {
+		allFlags[i].style.display = "none";
+	}
+}
+
+function showAllFlags() {
+	let allFlags = document.getElementsByClassName("flag");
+	for (let i = 0; i < allFlags.length; i++) {
+		allFlags[i].style.display = "";
+	}
+	document.title = pageTitle;
+}
+
+function showFlags() {
+	hideAllFlags();
+	
+	// Make the filter list from button inputs
+	let buttonInput = ""
+	for (k = 0; k < buttonArray.length; k++) {
+		buttonInput = buttonInput + " " + buttonArray[k];
+	}
+	
+	var list = "";
+	
+	if (buttonInput.length == 0 && textInput.length == 0) {
+		showAllFlags();
+	} else {
+		let list = textInput + buttonInput;
+		let visibleFlags = document.getElementsByClassName(list);
+		for (let n = 0; n < visibleFlags.length; n++) {
+			visibleFlags[n].style.display = "";
 		}
 	}
 	
-	/*
-	for (let i = 0; i < flags.length; i++) {
-		image = flags[i].getElementsByClassName("image");
-			if (splitinput.length == 1) {	//only one search term for quicker results
-				for (let k = 0; k < image[0].classList.length; k++) {
-					let textmatch = image[0].classList[k].toLowerCase().indexOf(input);
-					if (textmatch !== -1) {
-						flags[i].style.display = "";
-						break;
-					} else {
-						flags[i].style.display = "none";
-					}
-				}
-			} else {	//for more than one search term all of the words need to match perfectly
-				let textmatch = 0;
-				for (let n = 0; n < splitinput.length; n++) {
-					if (image[0].classList.contains(splitinput[n])) {
-						textmatch++;
-						if (textmatch == splitinput.length) {
-							flags[i].style.display = "";
-							break;
-						} else {
-							flags[i].style.display = "none";
-						}
-					}
-				}
-				
-			}
-	} */
+	// Set title to search terms
+	if (list.length > 0) {
+		document.title = "Flagfilter - " + list;
+	} else {
+		document.title = pageTitle;
+	}
+	
 }
 
-/* Adding class "active" to the button pressed */
-let myButtons = document.getElementById("myButtons");
-let filterbuttons = myButtons.getElementsByClassName("filterbutton");
-for (let i = 0; i < filterbuttons.length; i++) {
-	filterbuttons[i].addEventListener("click", function(){
-		let current = document.getElementsByClassName("active");
-		if (current.length == 0) {
-			this.className += " active";
-		} else {
-			current[0].classList.remove("active");
-			this.className += " active";
+function setActiveButton(input) {
+	if (input == "all") {
+		let myButtons = document.getElementById("myButtons");
+		let activeButtons = myButtons.getElementsByClassName("active");
+		
+		// Clear active status from all buttons
+		while(activeButtons.length > 0){
+			activeButtons[0].classList.remove("active");
 		}
-	});
+		
+		let newActiveButton = myButtons.getElementsByClassName("all");
+		newActiveButton[0].classList.add("active");		// Add active status to "Show all" button
+	} else {
+		let myButtons = document.getElementById("myButtons");
+		let filterbuttons = myButtons.getElementsByClassName("filterbutton");
+		filterbuttons[0].classList.remove("active");	// Clear active status of the "Show all" button
+		
+		let newActiveButton = myButtons.getElementsByClassName(input);
+	
+		if (newActiveButton.length > 0){
+			if (newActiveButton[0].classList.contains("active")) {
+			newActiveButton[0].classList.remove("active");			
+			} else {
+				newActiveButton[0].classList.add("active");
+			}
+		}
+		
+	}
+	
 }
-/*
-$(window).load(function () {
+
+// Popup using jQuery
+$(window).on('load', function() {
 	$(".trigger_popup_fricc").click(function(){
 		$('.hover_bkgr_fricc').show();
 	});
 	$('.hover_bkgr_fricc').click(function(){
 		$('.hover_bkgr_fricc').hide();
 	});
-});*/
-
-//autofocus the text input field
-let inputfield = document.getElementById("myInput");
-inputfield.focus();
+});
